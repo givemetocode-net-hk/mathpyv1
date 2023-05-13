4a9167967e6fa37d6fc6384583f4328106a19242
Jensenleung2465 <121221992+Jensenleung2465@users.noreply.github.com>
2023-05-13 16:57:36 +0800

    code

---
+++ example.py
@@ -0,0 +1,8 @@
+import mathpy
+"""
+# p(1,1) = Addition 1+1
+# o(3,2) = Subtraction 3-2
+# t(5,2) = multiplication 5x2
+# f(100,100) = Division 100/100
+# = 1
+"""
---
+++ mathpy.py
@@ -0,0 +1,8 @@
+def p(a,b) :
+    print(a+b)
+def o(a,b) :
+    print(a-b)
+def t(a,b) :
+    print(a*b)
+def f(a,b) :
+    print(a/b)
